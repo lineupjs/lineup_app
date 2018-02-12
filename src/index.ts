0b9b1139a4d8365b5d2e3f59f60005795febac07
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-02-12 01:11:50 +0100

    simple website

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import 'file-loader?name=index.html!./index.html';
+import './style.scss';
+import 'jquery';
+import 'imports-loader?jQuery=jquery!materialize-css';
+import { parse, ParseResult } from 'papaparse';
+import { builder, LineUp } from 'lineupjs';
+import 'lineupjs/build/LineUpJS.css';
+
+let lineup: LineUp;
+
+function uploadFile(file: File) {
+  return new Promise<ParseResult>((resolve) => {
+    parse(file, {
+      dynamicTyping: true,
+      header: true,
+      complete: resolve,
+      skipEmptyLines: true
+    });
+  });
+}
+function convertFile(result: ParseResult) {
+  lineup = builder(result.data)
+    .deriveColumns(...result.meta.fields)
+    .deriveColors()
+    .rowHeight(22, 2)
+    .defaultRanking()
+    .build(<HTMLElement>document.querySelector('div.lu'));
+}
+const uploader = <HTMLElement>document.querySelector('main');
+
+function showFile(file: File) {
+  uploader.dataset.state = 'uploading';
+  uploadFile(file)
+    .then(convertFile)
+    .then(() => new Promise<any>((resolve) => setTimeout(resolve, 500)))
+    .then(() => {
+      uploader.dataset.state = 'ready';
+    });
+}
+{
+  const file = (<HTMLInputElement>document.querySelector('input[type=file]'));
+  file.addEventListener('change', (evt) => {
+    showFile(file.files![0]);
+  });
+  (<HTMLElement>document.querySelector('#dropper a')).addEventListener('click', (evt) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+    file.click();
+  });
+  uploader.addEventListener('dragover', (evt) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+  });
+  uploader.addEventListener('drop', (evt) => {
+    if (evt.dataTransfer.files.length !== 1) {
+      return;
+    }
+    showFile(evt.dataTransfer.files[0]);
+    evt.preventDefault();
+  });
+}
