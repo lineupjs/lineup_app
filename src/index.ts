d05cc853b8d475de0d098a4291835f21b74196c0
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-02-16 17:46:12 +0100

    add codepen export

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,41 @@ import 'file-loader?name=index.html!./index.html';
 import './style.scss';
 import 'jquery';
 import 'imports-loader?jQuery=jquery!materialize-css';
-import { parse, ParseResult } from 'papaparse';
-import { builder, LineUp } from 'lineupjs';
+import {parse, ParseResult} from 'papaparse';
+import {builder, LineUp} from 'lineupjs';
 import 'lineupjs/build/LineUpJS.css';
 
 
 let lineup: LineUp;
 let uploadedFile: File;
 const uploader = <HTMLElement>document.querySelector('main');
+
 const downloadCSV = <HTMLLinkElement>document.querySelector('#downloadCSV');
 const downloadCSVHelper = <HTMLLinkElement>document.querySelector('#downloadCSVHelper');
+const createCodepen = <HTMLLinkElement>document.querySelector('#createCodePen');
+const createCodepenHelper = <HTMLFormElement>document.querySelector('#createCodePenHelper');
+
+const CODEPEN_CSS = `.lu {
+  clear: both;
+  position: absolute;
+  top: 1px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 0;
+}`;
+
+const CODEPEN_JS = `const arr = [];
+const cats = ['c1', 'c2', 'c3'];
+for (let i = 0; i < 100; ++i) {
+  arr.push({
+    a: Math.random() * 10,
+    d: 'Row ' + i,
+    cat: cats[Math.floor(Math.random() * 3)],
+    cat2: cats[Math.floor(Math.random() * 3)]
+  })
+}
+const lineup = LineUpJS.asLineUp(document.body, arr);`;
 
 function uploadFile(file: File) {
   return new Promise<ParseResult>((resolve) => {
@@ -40,6 +65,7 @@ function showFile(file: File) {
     .then(() => new Promise<any>((resolve) => setTimeout(resolve, 500)))
     .then(() => {
       downloadCSV.classList.remove('disabled');
+      createCodepen.classList.remove('disabled');
       uploader.dataset.state = 'ready';
     });
 }
@@ -53,6 +79,33 @@ downloadCSV.addEventListener('click', (evt) => {
     (<any>downloadCSVHelper).download = `${uploadedFile.name}.csv`;
     downloadCSVHelper.click();
   });
+});
+
+createCodepen.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  evt.stopPropagation();
+
+  const data = {
+    title: `LineUp ${uploadedFile.name}`,
+    description: 'this is an auto exported from the LineUp demo app',
+    html: ``,
+    css: CODEPEN_CSS,
+    css_pre_processor: 'scss',
+    css_starter: 'normalize',
+    js: CODEPEN_JS,
+    js_pre_processor: 'babel',
+    js_modernizr: false,
+    css_external: 'https://sgratzl.github.io/lineupjs_docs/master/LineUpJS.min.css',
+    js_external: 'https://sgratzl.github.io/lineupjs_docs/master/LineUpJS.min.js'
+  };
+
+  const json = JSON.stringify(data)
+    // Quotes will screw up the JSON
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;');
+
+  createCodepenHelper.querySelector('input')!.value = json;
+  createCodepenHelper.submit();
 });
 
 {
