a3b7cfad99dddf0cf98207eccfd633af7cc77402
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-02-18 02:47:16 +0100

    simple wurl use case

--- src/data/world-happiness-report/index.ts
+++ src/data/world-happiness-report/index.ts
@@ -1,5 +1,6 @@
 import {IDataset} from '../IDataset';
-import {builder} from 'lineupjs';
+import {parse, ParseResult} from 'papaparse';
+import {builder, buildRanking, buildStringColumn, buildNumberColumn} from 'lineupjs';
 
 export const data: IDataset = {
   id: 'happiness',
@@ -11,7 +12,7 @@ export const data: IDataset = {
   ranks 155 countries by their happiness levels, was released at the United Nations at an event celebrating
   International Day of Happiness on March 20th.</p>`,
   rawData: '',
-  buildScript: (rawVariable: string, domVariable: string) => {
+  buildScript(rawVariable: string, domVariable: string) {
     return `
     const parsed = Papa.parse(${rawVariable}, {
       dynamicTyping: true,
@@ -19,11 +20,54 @@ export const data: IDataset = {
       skipEmptyLines: true
     });
 
-    const lineup = LineUpJS.asLineUp(${domVariable}, parsed.data, ...parsed.meta.fields);
+    const lineup = LineUpJS.builder(parsed.data)
+      .column(LineUpJS.buildStringColumn('Country'))
+      .column(LineUpJS.buildNumberColumn('Happiness.Score', [0, 10]))
+      .column(LineUpJS.buildNumberColumn('Economy..GDP.per.Capita.', [0, 10]))
+      .column(LineUpJS.buildNumberColumn('Family', [0, 10]))
+      .column(LineUpJS.buildNumberColumn('Health..Life.Expectancy.', [0, 10]))
+      .column(LineUpJS.buildNumberColumn('Freedom', [0, 10]))
+      .column(LineUpJS.buildNumberColumn('Generosity', [0, 10]))
+      .column(LineUpJS.buildNumberColumn('Trust..Government.Corruption.', [0, 10]))
+      .column(LineUpJS.buildNumberColumn('Dystopia.Residual', [0, 10]))
+      .deriveColors()
+      .ranking(LineUpJS.buildRanking()
+        .supportTypes()
+        .allColumns()
+        .sortBy('Happiness.Score', 'desc')
+      )
+      .build(${domVariable});
     `;
   },
-  build: (node: HTMLElement) => {
-    return builder([]).build(node);
+  build(node: HTMLElement) {
+    return System.import('raw-loader!./2017.csv').then((content: any) => {
+      const csv: string = content.default ? content.default : content;
+      this.rawData = csv;
+      return parse(csv, {
+        dynamicTyping: true,
+        header: true,
+        skipEmptyLines: true
+      });
+    }).then((parsed: ParseResult) => {
+      // "Country","Happiness.Rank","Happiness.Score","Whisker.high","Whisker.low","Economy..GDP.per.Capita.","Family","Health..Life.Expectancy.","Freedom","Generosity","Trust..Government.Corruption.","Dystopia.Residual"
+      return builder(parsed.data)
+        .column(buildStringColumn('Country'))
+        .column(buildNumberColumn('Happiness.Score', [0, 10]))
+        .column(buildNumberColumn('Economy..GDP.per.Capita.', [0, 10]))
+        .column(buildNumberColumn('Family', [0, 10]))
+        .column(buildNumberColumn('Health..Life.Expectancy.', [0, 10]))
+        .column(buildNumberColumn('Freedom', [0, 10]))
+        .column(buildNumberColumn('Generosity', [0, 10]))
+        .column(buildNumberColumn('Trust..Government.Corruption.', [0, 10]))
+        .column(buildNumberColumn('Dystopia.Residual', [0, 10]))
+        .deriveColors()
+        .ranking(buildRanking()
+          .supportTypes()
+          .allColumns()
+          .sortBy('Happiness.Score', 'desc')
+        )
+        .build(node);
+    });
   }
 };
 
--- src/data/wur/index.ts
+++ src/data/wur/index.ts
@@ -1,9 +1,10 @@
 import {IDataset} from '../IDataset';
-import {builder} from 'lineupjs';
+import {parse, ParseResult} from 'papaparse';
+import {builder, buildRanking, buildStringColumn, buildNumberColumn} from 'lineupjs';
 
 export const data: IDataset = {
   id: 'wur',
-  title: 'World University Ranking',
+  title: 'Times Higher Education World University Ranking',
   image: '',
   link: 'https://www.kaggle.com/mylesoneill/world-university-rankings/version/3',
   description: `<p>
@@ -15,19 +16,60 @@ export const data: IDataset = {
   three global university rankings from very different places.
 </p>`,
   rawData: '',
-  buildScript: (rawVariable: string, domVariable: string) => {
-    return `
-  const parsed = Papa.parse(${rawVariable}, {
-    dynamicTyping: true,
-    header: true,
-    skipEmptyLines: true
-  });
-
-  const lineup = LineUpJS.asLineUp(${domVariable}, parsed.data, ...parsed.meta.fields);
-  `;
+  buildScript(_rawVariable: string, _domVariable: string) {
+    return `//TODO`;
   },
-  build: (node: HTMLElement) => {
-    return builder([]).build(node);
+  build(node: HTMLElement) {
+    return System.import('raw-loader!./cwurData.csv').then((content: any) => {
+      const csv: string = content.default ? content.default : content;
+      this.rawData = csv;
+      return parse(csv, {
+        dynamicTyping: true,
+        header: true,
+        skipEmptyLines: true
+      });
+    }).then((parsed: ParseResult) => {
+      const map = new Map<string, any>();
+      const years = new Set<string>();
+      parsed.data.forEach((d) => {
+        const i = d.institution;
+        if (!map.has(i)) {
+          map.set(i, d);
+        }
+        const v = map.get(i);
+        // year prefix stuff
+        const year = d.year;
+        years.add(year);
+        Object.keys(d).forEach((k) => {
+          const kk = `${year}_${k}`;
+          v[kk] = d[k];
+        });
+      });
+      const rows = Array.from(map.values());
+      const yearArray = Array.from(years).sort();
+      // world_rank,institution,country,national_rank,quality_of_education,alumni_employment,quality_of_faculty,publications,influence,citations,broad_impact,patents,year
+      // multiple years
+      // per year:
+      const perYear = `score,national_rank,quality_of_education,alumni_employment,quality_of_faculty,publications,influence,citations,broad_impact,patents`.split(',');
+      const b = builder(rows)
+        .column(buildStringColumn('institution'))
+        .column(buildStringColumn('country'));
+
+      yearArray.forEach((year) => {
+        const bb = buildRanking()
+          .supportTypes()
+          .column('institution')
+          .column('country')
+          .sortBy(`${year}_score`, 'desc');
+        perYear.forEach((k) => {
+          b.column(buildNumberColumn(`${year}_${k}`).label(`${k} (${year})`));
+          bb.column(`${year}_${k}`);
+        });
+        b.ranking(bb);
+      });
+
+      return b.deriveColors().build(node);
+    });
   }
 };
 
