2f6b1362b09c0f9dabbce52928b37b67c4e55ef4
Klaus Eckelt <Klaus.Eckelt@jku.at>
2018-08-10 08:51:15 +0200

    Fix linter errors

--- src/data/ieee-programming/index.ts
+++ src/data/ieee-programming/index.ts
@@ -1,7 +1,6 @@
 import {IDataset} from '../IDataset';
 import {parse, ParseResult} from 'papaparse';
 import {builder, buildRanking, buildStringColumn, buildCategoricalColumn, buildNumberColumn} from 'lineupjs';
-import { isNullOrUndefined } from 'util';
 
 import imageBars from './ieee_bars.png';
 import imageHeat from './ieee_heat.png';
@@ -25,50 +24,52 @@ export const ieeebars: IDataset = {
   });
 
   const years = [2014, 2015, 2016, 2017, 2018]; // 5 years
-      const sources = ['Career Builder', 'Dice', 'Github (active)', 'Github (created)', 'Google Search', 'Google Trends', 'Hacker News', 'IEEE Xplore', 'Reddit (posts)', 'Stack Overflow (?s)', 'Stack Overflow (views)', 'Twitter']; // 12 sources
+  const sources = ['Career Builder', 'Dice', 'Github (active)', 'Github (created)', 'Google Search', 'Google Trends', 'Hacker News', 'IEEE Xplore', 'Reddit (posts)', 'Stack Overflow (?s)', 'Stack Overflow (views)', 'Twitter']; // 12 sources
+  const spectrumWeights: { [s: string]: number; }  = {'Career Builder': 5, 'Dice': 5, 'Github (active)': 50, 'Github (created)': 30, 'Google Search': 50, 'Google Trends': 50, 'Hacker News': 20, 'IEEE Xplore': 100, 'Reddit (posts)': 20, 'Stack Overflow (?s)': 30, 'Stack Overflow (views)': 30, 'Twitter': 20};
 
-      // //Merge data of multiple years for each source
-      parsed.data.forEach((row) => {
-        sources.forEach((source) => {
-          row[source] = years.map((y) => !row[source + ' ' + y] && row[source + ' ' + y] !== 0 ? null : row[source + ' ' + y]);
-        });
-      });
+  // //Merge data of multiple years for each source
+  parsed.data.forEach((row) => {
+    sources.forEach((source) => {
+      row[source] = years.map((y) => !row[source + ' ' + year] && row[source + ' ' + year] !== 0 ? null : row[source + ' ' + year]);
+    });
+  });
 
-      // basic data
-      let dataBuilder = builder(parsed.data)
-        .column(buildStringColumn('name'))
-        .column(buildCategoricalColumn('web'))
-        .column(buildCategoricalColumn('mobile'))
-        .column(buildCategoricalColumn('enterprise'))
-        .column(buildCategoricalColumn('embedded'))
-        .column(buildNumberColumn('NA 2018')); // NA column of 2018
+  // basic data
+  let dataBuilder = builder(parsed.data)
+    .column(buildStringColumn('name'))
+    .column(buildCategoricalColumn('web'))
+    .column(buildCategoricalColumn('mobile'))
+    .column(buildCategoricalColumn('enterprise'))
+    .column(buildCategoricalColumn('embedded'))
+    .column(buildNumberColumn('NA 2018')); // NA column of 2018
 
-      // source matrices
-      sources.forEach((source) => {
-        dataBuilder = dataBuilder.column(buildNumberColumn(source).asArray(years.length))
-      });
+  // source matrices
+  sources.forEach((source) => {
+    dataBuilder = dataBuilder.column(buildNumberColumn(source).asArray(years.length));
+  });
 
-      // rankings per year, begin with 2018
-      years.reverse().forEach((year) => {
-        let columnNamesAndWeights: (string | number)[] = [];
+  // rankings per year, begin with 2018
+  years.reverse().forEach((year) => {
+    const columnNamesAndWeights: (string | number)[] = [];
 
-        sources.forEach((source) => {
-          const colName = source + ' ' + year;
-          dataBuilder = dataBuilder.column(buildNumberColumn(colName)) //Create a column to use it for the ranking
-          if (parsed.data[0] && parsed.data[0][colName]) // avoid NaN columns (e.g. Google Trends 2018)
-            columnNamesAndWeights.push(...[colName, 1]); // all columns have weight 1
-        });
+    sources.forEach((source) => {
+      const colName = source + ' ' + year;
+      dataBuilder = dataBuilder.column(buildNumberColumn(colName)); //Create a column to use it for the ranking
+      if (parsed.data[0] && parsed.data[0][colName]) { // avoid NaN columns (e.g. Google Trends 2018)
+        columnNamesAndWeights.push(...[colName, spectrumWeights[source]]); // all columns have weight 1
+      }
+    });
 
-        dataBuilder = dataBuilder.ranking(
-          buildRanking()
-            .weightedSum('' + year, <string>columnNamesAndWeights[0], <number>columnNamesAndWeights[1], <string>columnNamesAndWeights[2], <number>columnNamesAndWeights[3], ...columnNamesAndWeights.slice(4))
-            .supportTypes()
-            .column('name')
-            .sortBy('' + year, 'desc')
-        )
-      });
+    dataBuilder = dataBuilder.ranking(
+      buildRanking()
+        .weightedSum(''+year, <string>columnNamesAndWeights[0], <number>columnNamesAndWeights[1], <string>columnNamesAndWeights[2], <number>columnNamesAndWeights[3], ...columnNamesAndWeights.slice(4))
+        .supportTypes()
+        .column('name')
+        .sortBy(''+year, 'desc')
+    );
+  });
 
-      const lineup= dataBuilder.deriveColors().buildTaggle(${domVariable});
+  const lineup= dataBuilder.deriveColors().buildTaggle(${domVariable});
   `;
   },
   build(node: HTMLElement) {
@@ -83,12 +84,12 @@ export const ieeebars: IDataset = {
     }).then((parsed: ParseResult) => {
       const years = [2014, 2015, 2016, 2017, 2018]; // 5 years
       const sources = ['Career Builder', 'Dice', 'Github (active)', 'Github (created)', 'Google Search', 'Google Trends', 'Hacker News', 'IEEE Xplore', 'Reddit (posts)', 'Stack Overflow (?s)', 'Stack Overflow (views)', 'Twitter']; // 12 sources
-      const spectrum_weights: { [s: string]: number; }  = {'Career Builder': 5, 'Dice': 5, 'Github (active)': 50, 'Github (created)': 30, 'Google Search': 50, 'Google Trends': 50, 'Hacker News': 20, 'IEEE Xplore': 100, 'Reddit (posts)': 20, 'Stack Overflow (?s)': 30, 'Stack Overflow (views)': 30, 'Twitter': 20};
+      const spectrumWeights: { [s: string]: number; }  = {'Career Builder': 5, 'Dice': 5, 'Github (active)': 50, 'Github (created)': 30, 'Google Search': 50, 'Google Trends': 50, 'Hacker News': 20, 'IEEE Xplore': 100, 'Reddit (posts)': 20, 'Stack Overflow (?s)': 30, 'Stack Overflow (views)': 30, 'Twitter': 20};
 
       // //Merge data of multiple years for each source
       parsed.data.forEach((row) => {
         sources.forEach((source) => {
-          row[source] = years.map((y) => !row[source + ' ' + y] && row[source + ' ' + y] !== 0 ? null : row[source + ' ' + y]);
+          row[source] = years.map((y) => !row[`${source} ${y}`] && row[`${source} ${y}`] !== 0 ? null : row[`${source} ${y}`]);
         });
       });
 
@@ -111,19 +112,19 @@ export const ieeebars: IDataset = {
         const columnNamesAndWeights: (string | number)[] = [];
 
         sources.forEach((source) => {
-          const colName = source + ' ' + year;
+          const colName = `${source} ${year}`;
           dataBuilder = dataBuilder.column(buildNumberColumn(colName)); //Create a column to use it for the ranking
           if (parsed.data[0] && parsed.data[0][colName]) { // avoid NaN columns (e.g. Google Trends 2018)
-            columnNamesAndWeights.push(...[colName, spectrum_weights[source]]); // all columns have weight 1
+            columnNamesAndWeights.push(...[colName, spectrumWeights[source]]); // all columns have weight 1
           }
         });
 
         dataBuilder = dataBuilder.ranking(
           buildRanking()
-            .weightedSum('' + year, <string>columnNamesAndWeights[0], <number>columnNamesAndWeights[1], <string>columnNamesAndWeights[2], <number>columnNamesAndWeights[3], ...columnNamesAndWeights.slice(4))
+            .weightedSum(`${year}`, <string>columnNamesAndWeights[0], <number>columnNamesAndWeights[1], <string>columnNamesAndWeights[2], <number>columnNamesAndWeights[3], ...columnNamesAndWeights.slice(4))
             .supportTypes()
             .column('name')
-            .sortBy('' + year, 'desc')
+            .sortBy(`${year}`, 'desc')
         );
       });
 
@@ -151,50 +152,57 @@ export const ieeeheat: IDataset = {
   });
 
   const years = [2014, 2015, 2016, 2017, 2018]; // 5 years
-      const sources = ['Career Builder', 'Dice', 'Github (active)', 'Github (created)', 'Google Search', 'Google Trends', 'Hacker News', 'IEEE Xplore', 'Reddit (posts)', 'Stack Overflow (?s)', 'Stack Overflow (views)', 'Twitter']; // 12 sources
+  const sources = ['Career Builder', 'Dice', 'Github (active)', 'Github (created)', 'Google Search', 'Google Trends', 'Hacker News', 'IEEE Xplore', 'Reddit (posts)', 'Stack Overflow (?s)', 'Stack Overflow (views)', 'Twitter']; // 12 sources
 
-      // //Merge data of multiple years for each source
-      parsed.data.forEach((row) => {
-        sources.forEach((source) => {
-          row[source] = years.map((y) => !row[source + ' ' + y] && row[source + ' ' + y] !== 0 ? null : row[source + ' ' + y]);
-        });
-      });
+  parsed.data.forEach((row) => {
+    // //Merge data of multiple years for each source
+    sources.forEach((source) => {
+      row[source] = years.map((y) => row[source + ' ' + year]);
+    });
 
-      // basic data
-      let dataBuilder = builder(parsed.data)
-        .column(buildStringColumn('name'))
-        .column(buildCategoricalColumn('web'))
-        .column(buildCategoricalColumn('mobile'))
-        .column(buildCategoricalColumn('enterprise'))
-        .column(buildCategoricalColumn('embedded'))
-        .column(buildNumberColumn('NA 2018')); // NA column of 2018
+    // Merge data of multiple sources for each year
+    years.forEach((y) => {
+      row[y] = sources.map((source) => row[source + ' ' + year]);
+    });
+  });
 
-      // source matrices
-      sources.forEach((source) => {
-        dataBuilder = dataBuilder.column(buildNumberColumn(source).asArray(years.length))
-      });
 
-      // rankings per year, begin with 2018
-      years.reverse().forEach((year) => {
-        let columnNamesAndWeights: (string | number)[] = [];
+  // basic data
+  let dataBuilder = builder(parsed.data)
+    .column(buildStringColumn('name'))
+    .column(buildCategoricalColumn('web'))
+    .column(buildCategoricalColumn('mobile'))
+    .column(buildCategoricalColumn('enterprise'))
+    .column(buildCategoricalColumn('embedded'))
+    .column(buildNumberColumn('NA 2018')); // NA column of 2018
 
-        sources.forEach((source) => {
-          const colName = source + ' ' + year;
-          dataBuilder = dataBuilder.column(buildNumberColumn(colName)) //Create a column to use it for the ranking
-          if (parsed.data[0] && parsed.data[0][colName]) // avoid NaN columns (e.g. Google Trends 2018)
-            columnNamesAndWeights.push(...[colName, 1]); // all columns have weight 1
-        });
+  // source matrices
+  sources.forEach((source) => {
+    dataBuilder = dataBuilder.column(buildNumberColumn(source, [0, NaN]).asArray(years.length));
+  });
 
-        dataBuilder = dataBuilder.ranking(
-          buildRanking()
-            .weightedSum('' + year, <string>columnNamesAndWeights[0], <number>columnNamesAndWeights[1], <string>columnNamesAndWeights[2], <number>columnNamesAndWeights[3], ...columnNamesAndWeights.slice(4))
-            .supportTypes()
-            .column('name')
-            .sortBy('' + year, 'desc')
-        )
-      });
+  // year matrices
+  years.forEach((year) => {
+    dataBuilder = dataBuilder.column(buildNumberColumn('' + year, [0, NaN]).asArray(sources.length));
+  });
 
-      const lineup= dataBuilder.deriveColors().buildTaggle(${domVariable});
+  dataBuilder = dataBuilder.ranking(
+  buildRanking()
+      .supportTypes()
+      .column('name')
+      .column('2018')
+      .sortBy('2018', 'desc')
+    );
+
+  dataBuilder = dataBuilder.ranking(
+    buildRanking()
+        .supportTypes()
+        .column('name')
+        .column('2017').column('2016').column('2015').column('2014')
+        .sortBy('2017', 'desc')
+      );
+
+  const lineup= dataBuilder.deriveColors().buildTaggle(${domVariable});
   `;
   },
   build(node: HTMLElement) {
@@ -213,12 +221,12 @@ export const ieeeheat: IDataset = {
       parsed.data.forEach((row) => {
         // //Merge data of multiple years for each source
         sources.forEach((source) => {
-          row[source] = years.map((y) => row[source + ' ' + y]);
+          row[source] = years.map((y) => row[`${source} ${y}`]);
         });
 
         // Merge data of multiple sources for each year
         years.forEach((y) => {
-          row[y] = sources.map((source) => row[source + ' ' + y]);
+          row[y] = sources.map((source) => row[`${source} ${y}`]);
         });
       });
 
@@ -239,7 +247,7 @@ export const ieeeheat: IDataset = {
 
       // year matrices
       years.forEach((year) => {
-        dataBuilder = dataBuilder.column(buildNumberColumn(''+year, [0, NaN]).asArray(sources.length));
+        dataBuilder = dataBuilder.column(buildNumberColumn(`${year}`, [0, NaN]).asArray(sources.length));
       });
 
       dataBuilder = dataBuilder.ranking(
@@ -257,6 +265,7 @@ export const ieeeheat: IDataset = {
             .column('2017').column('2016').column('2015').column('2014')
             .sortBy('2017', 'desc')
           );
+
       return dataBuilder.deriveColors().buildTaggle(node);
     });
   }
