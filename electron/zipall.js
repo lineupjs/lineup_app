32eb5eb1160d019a3217b9ac47446ffa9bca58c0
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-11-25 10:51:35 +0100

    build to tmp and not dist

--- electron/zipall.js
+++ electron/zipall.js
@@ -2,27 +2,28 @@ const zip = require('electron-installer-zip');
 const {readdirSync, statSync} = require('fs');
 const {join} = require('path');
 
-const base = join(__dirname, '../dist');
-const dirs = readdirSync(base).filter((f) => statSync(join(base, f)).isDirectory());
+const tmp = join(__dirname, '../tmp');
+const dist = join(__dirname, '../dist');
+const dirs = readdirSync(tmp).filter((f) => statSync(join(tmp, f)).isDirectory());
 
 dirs.forEach((dir) => {
   zip({
-    dir: join(base, dir),
-    out: join(base, dir + '.zip')
+    dir: join(tmp, dir),
+    out: join(dist, dir + '.zip')
   }, function(err, res) {
     if (err) {
       console.error(err);
     }
-    console.log('Zip file written to: ', join(base, dir + '.zip'));
+    console.log('Zip file written to: ', join(dist, dir + '.zip'));
   });
 });
 
 zip({
   dir: join(__dirname, './app/build'),
-  out: join(base, 'LineUpApp.zip')
+  out: join(dist, 'LineUpApp.zip')
 }, function(err) {
   if (err) {
     console.error(err);
   }
-  console.log('Zip file written to: ', join(base, 'LineUpApp.zip'));
+  console.log('Zip file written to: ', join(dist, 'LineUpApp.zip'));
 });
